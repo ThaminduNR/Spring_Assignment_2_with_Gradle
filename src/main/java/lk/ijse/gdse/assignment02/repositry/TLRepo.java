42b5c09a1dfe41da987f16715bb6c20f907ec1ab
ThaminduNR <thaminduniwantha777@gmail.com>
2023-09-23 22:17:17 +0530

    update repo

--- src/main/java/lk/ijse/gdse/assignment02/repositry/TLRepo.java
+++ src/main/java/lk/ijse/gdse/assignment02/repositry/TLRepo.java
@@ -2,8 +2,9 @@ package lk.ijse.gdse.assignment02.repositry;
 
 import lk.ijse.gdse.assignment02.entity.TL;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface TLRepo extends JpaRepository<TL, UUID> {
 }
